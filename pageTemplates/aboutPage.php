829bf531029ebd3656f68c030e94845bab4380b1
Kamil Ogrodowczyk <kamil.web.development@gmail.com>
2023-03-11 14:37:02 +0100

    Add single page and about page

--- pageTemplates/aboutPage.php
+++ pageTemplates/aboutPage.php
@@ -7,9 +7,79 @@
 
 ?>
 
-    <main>
+    <main id="about" class="about">
         
         <?php get_template_part('/libs/pageHeader'); ?>
+
+        <section class="about-wrapper">
+            <div class="about-text-icons">
+                <div class="about-text">
+                    <p>MP-TECHNIK jest prężnie rozwijającą się firmą na terenie Polski i Europy. Zaopatrujemy naszych Klientów w odpowiednio dobrany asortyment maszyn, urządzeń i akcesoriów dla określonej branży. Działamy na wielu płaszczyznach: energetyka, teletechnika,wod-kan.
+                    <br/><br/>
+                    Nieustannie się rozwijamy oraz pragniemy zostać ekspertami w najmniejszym calu w każdej dziedzinie naszej działalności. Świadczymy pełen zakres usług w obszarze przeglądów oraz przeprowadzamy różnego rodzaju renowacje oraz naprawy m.in. urządzeń do przecisków, młotów do wbijania rur stalowych, kompresorów i wciągarek linowych. Jesteśmy subdealerem firmy JL MASKINER w zakresie sprzedaży maszyn do przecisków marki HAMMERHEAD.</p>
+                </div>
+                <div class="about-icons">
+                    <div class="about-icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32.003" height="48" viewBox="0 0 32.003 48">
+                            <g id="layer1" transform="translate(-3.174 -280.596)">
+                                <path id="path31836" d="M22.336,280.6a.8.8,0,0,0-.614.342l-18.4,26.4a.8.8,0,0,0,.656,1.258H16.707l-1.528,19.139a.8.8,0,0,0,1.452.52l18.4-26.4a.8.8,0,0,0-.655-1.258H21.645l1.53-19.141a.8.8,0,0,0-.839-.863Z" transform="translate(0)" fill="#3592ee"/>
+                            </g>
+                        </svg>
+                        <p>energetyka</p>
+                    </div>
+                    <div class="about-icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="47.375" height="48" viewBox="0 0 47.375 48">
+                            <path id="station" d="M26.212,27.9l15,22.5h-30Zm-2.75-6a3.866,3.866,0,0,1-.25-5.25,3.72,3.72,0,0,1,5.5-.25h.25a3.624,3.624,0,0,1-.25,5.25A3.866,3.866,0,0,1,23.462,21.9Zm-14-19.5,3.5,3.5a18.862,18.862,0,0,0,0,26.5l-3.5,3.5a23.689,23.689,0,0,1,0-33.5Zm33.5,0a23.689,23.689,0,0,1,0,33.5l-3.5-3.5a18.862,18.862,0,0,0,0-26.5Zm-26.5,7,3.5,3.5a8.457,8.457,0,0,0,0,12.25l-3.5,3.75A14.04,14.04,0,0,1,16.462,9.4Zm19.5,0a14.039,14.039,0,0,1,0,19.5l-3.5-3.5a8.457,8.457,0,0,0,0-12.25c0-.25,3.5-3.75,3.5-3.75Z" transform="translate(-2.525 -2.4)" fill="#3592ee"/>
+                        </svg>
+                        <p>teletechnika</p>
+                    </div>
+                    <div class="about-icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="34.936" height="48" viewBox="0 0 34.936 48">
+                            <path id="Path_1287" data-name="Path 1287" d="M24.778.373a1,1,0,0,0-1.746.376c-.207.392-1.225,1.778-2.3,3.243-4.962,6.757-14.2,19.335-14.2,26.538a17.468,17.468,0,1,0,34.936,0c0-9.34-16.008-29.312-16.69-30.157Z" transform="translate(-6.532 0)" fill="#3592ee"/>
+                        </svg>
+                        <p>wod-kan</p>
+                    </div>
+                </div>
+            </div>
+            <div class="about-images">
+                <div class="about-image">
+                    <img src="<?php echo THEME_URL; ?>_dev/img/working-area-modern-office.jpg" alt="nowoczesne biuro">
+                    <img src="<?php echo THEME_URL; ?>_dev/img/minimalist-empty-room.jpg" alt="minimalistyczne pomieszczenie">
+                    <img src="<?php echo THEME_URL; ?>_dev/img/mptechnik-product.jpg" alt="sprężarka-kompresor">
+                    <img src="<?php echo THEME_URL; ?>_dev/img/large-modern-warehouse-building.jpg" alt="nowoczesna hala">
+                </div>
+            </div>
+            <div class="about-experience">
+                <h2 class="mb-100">
+                    wieloletnie
+                    <div>doświadczenie</div>
+                </h2>
+                <div class="about-text">
+                    <p>Posiadamy wieloletnie doświadczenie oraz szeroką wiedzę specjalistyczną. Liczne certyfikaty czołowych instytutów oraz ośrodków badawczo – szkoleniowych są najlepszym oraz zauważalnym potwierdzeniem naszych kompetencji. Niewątpliwie zależy nam na dobru Klienta, dlatego produkty proponowane naszym konsumentom spełniają najwyższe panujące normy. Zachęcamy do skorzystania z ofert firmy MP-TECHNIK, a następnie wypełnienia formularzu zgłoszeniowego, który znajduje się w kontakcie.</p>
+                </div>
+            </div>
+            <div class="about-knowledge">
+                <div class="about-knowledge-title">
+                    <h2 class="mb-100">
+                        specjalistyczna
+                        <div>wiedza</div>
+                    </h2>
+                </div>
+                <div class="about-image">
+                    <img src="<?php echo THEME_URL; ?>_dev/img/certificate-1.jpg" alt="certyfikat ukończenia kursu kwalifikującego">
+                    <img src="<?php echo THEME_URL; ?>_dev/img/certificate-2.jpg" alt="certyfikat ukończenia kursu rachunkowości">
+                    <img src="<?php echo THEME_URL; ?>_dev/img/certificate-1.jpg" alt="certyfikat ukończenia kursu kwalifikującego">
+                    <img src="<?php echo THEME_URL; ?>_dev/img/certificate-2.jpg" alt="certyfikat ukończenia kursu rachunkowości">
+                </div>
+                <div class="about-knowledge--bottom">
+                    <a href = "#" class="contact-btn">
+                        <span>kontakt</span>
+                    </a>
+                </div>
+            </div>
+
+            <?php get_template_part('/libs/advantages'); ?>
+        </section>
         
     </main>
 
--- single.php
+++ single.php
@@ -1,7 +1,94 @@
 <?php get_header(); ?>
 
-    <main>
-        single.php
+    <main id="single" class="single">
+        <?php get_template_part('/libs/pageHeader'); ?>
+
+        <section class="single-wrapper">
+            <div class="single-img">
+                <img src="<?php echo THEME_URL; ?>_dev/img/event.jpg" alt="wydarzenie">
+            </div>
+            <div class="single-text">
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit</p>
+            </div>
+
+            <div class="single-product-info">
+                <div class="product-info--left">
+                    <div class="product-info-img">
+                        <img src="<?php echo THEME_URL; ?>_dev/img/product-info.jpg" alt="HAMMERHEAD 4">
+                    </div>
+                    <div class="product-info-text">
+                        <h4>Dane techniczne</h4>
+                        <p>model: HAMMERHEAD 4”</p>
+                        <p>średnica przecisku : 98 mm (z poszerzeniem 180 mm)</p>
+                        <p>długość przecisku : 1624 mm</p>
+                        <p>waga : 62 kg</p>
+                        <p>pasywna głowica przecisku</p>
+                        <p>zapotrzebowanie na powietrze : 1926 L/min</p>
+                        <p>ciśnienie robocze :7,6 bar</p>
+                        <p>praca: przód/tył, zmiana kierunku przez obrót węża</p>
+                    </div>
+                </div>
+                <div class="product-info--right">
+                    <div class="product-info-img">
+                        <img src="<?php echo THEME_URL; ?>_dev/img/event-product-info.jpg" alt="wydarzenie">
+                    </div>
+                    <div class="product-info-text">
+                        <h4>W skład zestawu wchodzi</h4>
+                        <p>maszyna przeciskowa HAMMERHEAD 2,5” (67mm)</p>
+                        <p>wąż roboczy 3/4″ X 15 M</p>
+                        <p>oliwiarka</p>
+                        <p>zestaw kluczy</p>
+                        <p>przyrząd do ustawiania maszyny</p>
+                        <p>olej do maszyn przeciskowych</p>
+                    </div>
+                </div>
+            </div>
+            <div class="single-social-img">
+                <img src="<?php echo THEME_URL; ?>_dev/img/post-1.jpg" alt="Hammerhead 2.5 post">
+                <img src="<?php echo THEME_URL; ?>_dev/img/post-2.jpg" alt="Terma max K130S post">
+            </div>
+            <div class="single-images">
+                <div class="single-image">
+                    <img src="<?php echo THEME_URL; ?>_dev/img/single-photo-1.jpg" alt="dyskusja">
+                    <img src="<?php echo THEME_URL; ?>_dev/img/single-photo-2.jpg" alt="wykopalisko">
+                    <img src="<?php echo THEME_URL; ?>_dev/img/single-photo-1.jpg" alt="dyskusja">
+                    <img src="<?php echo THEME_URL; ?>_dev/img/single-photo-2.jpg" alt="wykopalisko">
+                </div>
+                <div class="single-image">
+                    <img src="<?php echo THEME_URL; ?>_dev/img/single-photo-1.jpg" alt="dyskusja">
+                    <img src="<?php echo THEME_URL; ?>_dev/img/single-photo-2.jpg" alt="wykopalisko">
+                    <img src="<?php echo THEME_URL; ?>_dev/img/single-photo-1.jpg" alt="dyskusja">
+                    <img src="<?php echo THEME_URL; ?>_dev/img/single-photo-2.jpg" alt="wykopalisko">
+                </div>
+
+                <div class="single-images--bottom">
+                    <a href = "<?php echo esc_url(home_url('/aktualnosci')) ?>" class="back-btn">
+                        <span>wróć</span>
+                    </a>
+                    <div class="pageHeader--socials">
+                        <a href="#" class="pageHeader--social">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="9.919" height="19.102" viewBox="0 0 9.919 19.102">
+                                <g id="facebook-app-symbol" transform="translate(-37.29)">
+                                    <path id="f_1_" d="M43.729,19.1V10.39h2.923l.439-3.4H43.729V4.825c0-.983.272-1.653,1.683-1.653h1.8V.133A24.368,24.368,0,0,0,44.59,0C42,0,40.222,1.583,40.222,4.488v2.5H37.29v3.4h2.932V19.1Z" fill="#3592ee"/>
+                                </g>
+                            </svg>
+                        </a>
+                        <a href="#" class="pageHeader--social">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20.742" height="14.694" viewBox="0 0 20.742 14.694">
+                                <g id="youtube" transform="translate(0 -59.72)">
+                                    <g id="Group_2" data-name="Group 2" transform="translate(0 59.72)">
+                                    <path id="Path_8" data-name="Path 8" d="M20.453,62.124a2.317,2.317,0,0,0-1.86-1.9,66.659,66.659,0,0,0-16.442,0,2.316,2.316,0,0,0-1.861,1.9,42.379,42.379,0,0,0,0,9.884,2.316,2.316,0,0,0,1.861,1.9,66.66,66.66,0,0,0,16.442,0,2.315,2.315,0,0,0,1.86-1.9A42.373,42.373,0,0,0,20.453,62.124Zm-11.811,8.4V63.61l5.186,3.457Z" transform="translate(0 -59.72)" fill="#3592ee"/>
+                                    </g>
+                                </g>
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <?php get_template_part('/libs/home/primaryCategories'); ?>
+        </section>
     </main>
 
 <?php get_footer(); ?>
