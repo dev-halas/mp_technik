6f0ed6444ace0460cf5f640b3cd57001d7a0a205
Kamil Ogrodowczyk <kamil.web.development@gmail.com>
2023-04-05 21:16:28 +0200

    Single page fix styles; import images with real sizes for gallery

--- single.php
+++ single.php
@@ -13,22 +13,22 @@
         </div>
     </header>
 
-    <main id="single" class="single">
-        <section class="single-wrapper">
+    <main id="single">
+        <section class="container">
             <div class="single-img">
                 <img src="<?php echo THEME_URL; ?>_dev/img/event.jpg" alt="wydarzenie">
             </div>
             <div class="single-text">
                 <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit</p>
+                <br/><br/>
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
             </div>
 
             <div class="single-product-info">
-                <div class="product-info-img first-img">
+                <div class="product-info-img">
                     <img src="<?php echo THEME_URL; ?>_dev/img/product-info.jpg" alt="HAMMERHEAD 4">
                 </div>
-                <div class="product-info-text first-text">
+                <div class="product-info-text">
                     <h4>Dane techniczne</h4>
                     <p>model: HAMMERHEAD 4”</p>
                     <p>średnica przecisku : 98 mm (z poszerzeniem 180 mm)</p>
@@ -39,10 +39,9 @@
                     <p>ciśnienie robocze :7,6 bar</p>
                     <p>praca: przód/tył, zmiana kierunku przez obrót węża</p>
                 </div>
-                <div class="product-info-img second-img">
-                    <img src="<?php echo THEME_URL; ?>_dev/img/event-product-info.jpg" alt="wydarzenie">
-                </div>
-                <div class="product-info-text second-text">
+            </div>
+            <div class="single-product-info">
+                <div class="product-info-text">
                     <h4>W skład zestawu wchodzi</h4>
                     <p>maszyna przeciskowa HAMMERHEAD 2,5” (67mm)</p>
                     <p>wąż roboczy 3/4″ X 15 M</p>
@@ -51,66 +50,78 @@
                     <p>przyrząd do ustawiania maszyny</p>
                     <p>olej do maszyn przeciskowych</p>
                 </div>
+                <div class="product-info-img">
+                    <img src="<?php echo THEME_URL; ?>_dev/img/event-product-info.jpg" alt="wydarzenie">
+                </div>
             </div>
-            <div class="single-social-img">
-                <div>
-                    <img src="<?php echo THEME_URL; ?>_dev/img/drippex1.jpg" alt="Hammerhead 2.5 post">
+            <div class="single-decorative-images-text">
+                <h4>Lorem ipsum dolor sit amet</h4>
+                <div class="single-decorative-images">
+                    <div class="single-decorative-image">
+                        <img src="<?php echo THEME_URL; ?>_dev/img/drippex1.jpg" alt="Hammerhead 2.5 post">
+                    </div>
+                    <div class="single-decorative-image">
+                        <img src="<?php echo THEME_URL; ?>_dev/img/drippex2.jpg" alt="Terma max K130S post">
+                    </div>
                 </div>
-                <div>
-                    <img src="<?php echo THEME_URL; ?>_dev/img/drippex2.jpg" alt="Terma max K130S post">
-                </div>
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                <br/><br/>
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
             </div>
             <div class="single-gallery-container">
+                <h4>Lorem ipsum dolor sit amet</h4>
                 <div id="lightGallery" class="single-images">
                     <div class="single-image" data-src="<?php echo THEME_URL; ?>_dev/img/single-photo-1.jpg">
-                            <img src="<?php echo THEME_URL; ?>_dev/img/single-photo-1.jpg" alt="dyskusja">
-                        </div>
-                        <div class="single-image" data-src="<?php echo THEME_URL; ?>_dev/img/single-photo-2.jpg">
-                            <img src="<?php echo THEME_URL; ?>_dev/img/single-photo-2.jpg" alt="wykopalisko">
-                        </div>
-                        <div class="single-image" data-src="<?php echo THEME_URL; ?>_dev/img/single-photo-1.jpg">
-                            <img src="<?php echo THEME_URL; ?>_dev/img/single-photo-1.jpg" alt="dyskusja">
-                        </div>
-                        <div class="single-image" data-src="<?php echo THEME_URL; ?>_dev/img/single-photo-2.jpg">
-                            <img src="<?php echo THEME_URL; ?>_dev/img/single-photo-2.jpg" alt="wykopalisko">
-                        </div>
-                        <div class="single-image" data-src="<?php echo THEME_URL; ?>_dev/img/single-photo-1.jpg">
-                            <img src="<?php echo THEME_URL; ?>_dev/img/single-photo-1.jpg" alt="dyskusja">
-                        </div>
-                        <div class="single-image" data-src="<?php echo THEME_URL; ?>_dev/img/single-photo-2.jpg">
-                            <img src="<?php echo THEME_URL; ?>_dev/img/single-photo-2.jpg" alt="wykopalisko">
-                        </div>
-                        <div class="single-image" data-src="<?php echo THEME_URL; ?>_dev/img/single-photo-1.jpg">
-                            <img src="<?php echo THEME_URL; ?>_dev/img/single-photo-1.jpg" alt="dyskusja">
-                        </div>
-                        <div class="single-image" data-src="<?php echo THEME_URL; ?>_dev/img/single-photo-2.jpg">
-                            <img src="<?php echo THEME_URL; ?>_dev/img/single-photo-2.jpg" alt="wykopalisko">
-                        </div>
+                        <img src="<?php echo THEME_URL; ?>_dev/img/single-photo-1.jpg" alt="dyskusja">
                     </div>
-                    <div class="single-images--bottom">
-                        <a href = "<?php echo esc_url(home_url('/aktualnosci')) ?>" class="back-btn">
-                            <span>wróć</span>
-                        </a>
-                        <div class="pageHeader--socials">
-                            <a href="#" class="pageHeader--social">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="9.919" height="19.102" viewBox="0 0 9.919 19.102">
-                                    <g id="facebook-app-symbol" transform="translate(-37.29)">
-                                        <path id="f_1_" d="M43.729,19.1V10.39h2.923l.439-3.4H43.729V4.825c0-.983.272-1.653,1.683-1.653h1.8V.133A24.368,24.368,0,0,0,44.59,0C42,0,40.222,1.583,40.222,4.488v2.5H37.29v3.4h2.932V19.1Z" fill="#3592ee"/>
-                                    </g>
-                                </svg>
-                            </a>
-                            <a href="#" class="pageHeader--social">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20.742" height="14.694" viewBox="0 0 20.742 14.694">
-                                    <g id="youtube" transform="translate(0 -59.72)">
-                                        <g id="Group_2" data-name="Group 2" transform="translate(0 59.72)">
-                                        <path id="Path_8" data-name="Path 8" d="M20.453,62.124a2.317,2.317,0,0,0-1.86-1.9,66.659,66.659,0,0,0-16.442,0,2.316,2.316,0,0,0-1.861,1.9,42.379,42.379,0,0,0,0,9.884,2.316,2.316,0,0,0,1.861,1.9,66.66,66.66,0,0,0,16.442,0,2.315,2.315,0,0,0,1.86-1.9A42.373,42.373,0,0,0,20.453,62.124Zm-11.811,8.4V63.61l5.186,3.457Z" transform="translate(0 -59.72)" fill="#3592ee"/>
-                                        </g>
-                                    </g>
-                                </svg>
-                            </a>
-                        </div>
+                    <div class="single-image" data-src="<?php echo THEME_URL; ?>_dev/img/single-photo-2.jpg">
+                        <img src="<?php echo THEME_URL; ?>_dev/img/single-photo-2.jpg" alt="wykopalisko">
+                    </div>
+                    <div class="single-image" data-src="<?php echo THEME_URL; ?>_dev/img/single-photo-1.jpg">
+                        <img src="<?php echo THEME_URL; ?>_dev/img/single-photo-1.jpg" alt="dyskusja">
+                    </div>
+                    <div class="single-image" data-src="<?php echo THEME_URL; ?>_dev/img/single-photo-2.jpg">
+                        <img src="<?php echo THEME_URL; ?>_dev/img/single-photo-2.jpg" alt="wykopalisko">
+                    </div>
+                    <div class="single-image" data-src="<?php echo THEME_URL; ?>_dev/img/single-photo-1.jpg">
+                        <img src="<?php echo THEME_URL; ?>_dev/img/single-photo-1.jpg" alt="dyskusja">
+                    </div>
+                    <div class="single-image" data-src="<?php echo THEME_URL; ?>_dev/img/single-photo-2.jpg">
+                        <img src="<?php echo THEME_URL; ?>_dev/img/single-photo-2.jpg" alt="wykopalisko">
+                    </div>
+                    <div class="single-image" data-src="<?php echo THEME_URL; ?>_dev/img/single-photo-1.jpg">
+                        <img src="<?php echo THEME_URL; ?>_dev/img/single-photo-1.jpg" alt="dyskusja">
+                    </div>
+                    <div class="single-image" data-src="<?php echo THEME_URL; ?>_dev/img/single-photo-2.jpg">
+                        <img src="<?php echo THEME_URL; ?>_dev/img/single-photo-2.jpg" alt="wykopalisko">
                     </div>
                 </div>
+                <div class="single-images--bottom">
+                    <a href = "<?php echo esc_url(home_url('/aktualnosci')) ?>" class="back-btn">
+                        <span>wróć</span>
+                    </a>
+                    <div class="pageHeader--socials">
+                        <a href="#" class="pageHeader--social">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="9.919" height="19.102" viewBox="0 0 9.919 19.102">
+                                <g id="facebook-app-symbol" transform="translate(-37.29)">
+                                    <path id="f_1_" d="M43.729,19.1V10.39h2.923l.439-3.4H43.729V4.825c0-.983.272-1.653,1.683-1.653h1.8V.133A24.368,24.368,0,0,0,44.59,0C42,0,40.222,1.583,40.222,4.488v2.5H37.29v3.4h2.932V19.1Z" fill="#3592ee"/>
+                                </g>
+                            </svg>
+                        </a>
+                        <a href="#" class="pageHeader--social">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20.742" height="14.694" viewBox="0 0 20.742 14.694">
+                                <g id="youtube" transform="translate(0 -59.72)">
+                                    <g id="Group_2" data-name="Group 2" transform="translate(0 59.72)">
+                                    <path id="Path_8" data-name="Path 8" d="M20.453,62.124a2.317,2.317,0,0,0-1.86-1.9,66.659,66.659,0,0,0-16.442,0,2.316,2.316,0,0,0-1.861,1.9,42.379,42.379,0,0,0,0,9.884,2.316,2.316,0,0,0,1.861,1.9,66.66,66.66,0,0,0,16.442,0,2.315,2.315,0,0,0,1.86-1.9A42.373,42.373,0,0,0,20.453,62.124Zm-11.811,8.4V63.61l5.186,3.457Z" transform="translate(0 -59.72)" fill="#3592ee"/>
+                                    </g>
+                                </g>
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                <br/><br/>
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
             </div>
             <?php get_template_part('/libs/home/primaryCategories'); ?>
         </section>
